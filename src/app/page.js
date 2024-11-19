ad580ec41c1c2d4ff8a3b49ee1e7bb2e40fdf716
Stephanie Udemezue <udemezuestephanie6@gmail.com>
2024-11-20 00:10:33 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      Home
+      {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"> */}
+       Home
+
     </div>
   );
 }
