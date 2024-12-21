74b7541de39fc8c75fdf0e3e6d7f23aa0b1152ff
Stephanie Udemezue <udemezuestephanie6@gmail.com>
2024-12-21 05:55:04 +0100

    add mongoose update

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,43 +1,85 @@
-import { clerkClient } from "@clerk/nextjs/dist/types/server";
+// import { clerkClient } from "@clerk/nextjs/dist/types/server";
+// import User from "../models/user.model";
+// import { connect } from "../mongodb/mongoose";
+
+// export const createOrUpdateUser = async (
+//   id,
+//   first_name,
+//   last_name,
+//   img_url,
+//   email_addresses,
+//   username
+// ) => {
+//   try {
+//     await connect();
+
+//     const user = await User.findOneAndUpdate(
+//       { clerkId: id },
+//       {
+//         $set: {
+//           firstName: first_name,
+//           lastName: last_name,
+//           email: email_addresses[0].email_address,
+//           profilePicture: img_url,
+//           username: username,
+//         },
+//       },
+//       { new: true, upsert: true }
+//     );
+
+//     return user;
+//   } catch (error) {
+//     console.error("Error creating or updating user", error);
+//   }
+// };
+
+// export const deleteUser = async () => {
+//   try {
+//     await connect();
+//     await User.findOneAndDelete({ clerkId: id });
+//   } catch (error) {
+//     console.error("Error deleting user", error);
+//   }
+// };
+
 import User from "../models/user.model";
+
 import { connect } from "../mongodb/mongoose";
 
 export const createOrUpdateUser = async (
   id,
   first_name,
   last_name,
-  img_url,
+  image_url,
   email_addresses,
   username
 ) => {
   try {
     await connect();
-
     const user = await User.findOneAndUpdate(
       { clerkId: id },
       {
         $set: {
           firstName: first_name,
           lastName: last_name,
+          profilePicture: image_url,
           email: email_addresses[0].email_address,
-          profilePicture: img_url,
-          username: username,
+          username,
         },
       },
       { new: true, upsert: true }
     );
-
     return user;
   } catch (error) {
-    console.error("Error creating or updating user", error);
+    console.log("Error creating or updating user:", error);
   }
 };
 
-export const deleteUser = async () => {
+export const deleteUser = async (id) => {
   try {
     await connect();
     await User.findOneAndDelete({ clerkId: id });
   } catch (error) {
-    console.error("Error deleting user", error);
+    console.log("Error deleting user:", error);
   }
 };
--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -1,17 +1,56 @@
+// import mongoose from "mongoose";
+
+// const userShema = new mongoose.Schema(
+//   {
+//     clerkId: {
+//       type: String,
+//       required: true,
+//       undefined: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       undefined: true,
+//     },
+//     firstName: {
+//       type: String,
+//       required: true,
+//     },
+//     lastName: {
+//       type: String,
+//       required: true,
+//     },
+//     profilePicture: {
+//       type: String,
+//       required: true,
+//     },
+//     isAdmin: {
+//       type: Boolean,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// const User = mongoose.models.User || mongoose.model("mongoose", userShema);
+// export default User;
+
 import mongoose from "mongoose";
 
-const userShema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     clerkId: {
       type: String,
       required: true,
-      undefined: true,
+      unique: true,
     },
+
     email: {
       type: String,
       required: true,
-      undefined: true,
+      unique: true,
     },
+
     firstName: {
       type: String,
       required: true,
@@ -20,17 +59,24 @@ const userShema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    profilePicture: {
+    username: {
       type: String,
       required: true,
+      unique: true,
     },
+
+    profilePicture: {
+      type: String,
+      required: false,
+    },
+
     isAdmin: {
       type: Boolean,
-      required: true,
+      default: false,
     },
   },
   { timestamps: true }
 );
 
-const User = mongoose.models.User || mongoose.model("mongoose", userShema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
